eb96ade957e636309913ae9c507553b4f18e441c
Andrew Bloom <blooma116@gmail.com>
2017-12-29 17:47:45 -0800

    working on mock factories

--- __test__/lib/setup.js
+++ __test__/lib/setup.js
@@ -10,3 +10,31 @@ process.env.SECRET_SALT = 'Password123!';
 process.env.MONGODB_URI = 'mongodb://localhost/testing';
 process.env.PORT = 3000;
 
+awsSDKMock.mock('S3', 'upload', (params, callback) => {
+  if (!params.Key || !params.Bucket || !params.Body || !params.ACL){
+    return callback(new Error('__ERROR__', 'key, bucket, body and ACL are required'));
+  }
+
+  if (params.ACL !== 'public-read'){
+    return callback(new Error('__ERROR__', 'ACL should be public-read'));
+  }
+
+  if (params.Bucket !== process.env.AWS_BUCKET){
+    return callback(new Error('__ERROR__', 'bucket is incorrect'));
+  }
+
+  callback(null, {Location: faker.internet.url()});
+});
+
+awsSDKMock.mock('S3', 'deleteObject', (params, callback) => {
+  if (!params.Key || !params.Bucket){
+    return callback(new Error('__ERROR__', 'key, and bucket are required'));
+  }
+
+  if (params.Bucket !== process.env.AWS_BUCKET) {
+    return callback(new Error('__ERROR__', 'bucket is incorrect'));
+  }
+
+  callback(null, {});
+});
+
