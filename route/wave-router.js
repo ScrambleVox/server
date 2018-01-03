ef9de148f769c9969613d1a33c0f414ccdc5952a
David Lindahl <davidalindahl@gmail.com>
2018-01-03 10:09:28 -0800

    wave router for bitcrusher POST route

--- route/wave-router.js
+++ route/wave-router.js
@@ -3,6 +3,7 @@
 // ROUTER
 const {Router} = require('express');
 const httpErrors = require('http-errors');
+const fsx = require('fs-extra');
 const bearerAuth = require('../lib/middleware/bearer-auth');
 const waveParser = require('../lib/transforms/wave-parser');
 const bitCrusher = require('../lib/transforms/bitcrusher');
@@ -16,15 +17,42 @@ const S3 = require('../lib/middleware/s3');
 const waveRouter = module.exports = new Router();
 
 // TODO: make each post specific to the transform.
-waveRouter.post('/waves', bearerAuth, upload.any(), (request, response, next) => {
+waveRouter.post('/waves/:transform', bearerAuth, upload.any(), (request, response, next) => {
   if(!request.user)
     return next(new httpErrors(404, '__ERROR__ not found'));
 
   if(!request.body.wavename || request.files.length > 1 || request.files[0].fieldname !== 'wave')
     return next(new httpErrors(400, '__ERROR__ invalid request'));
 
-  let file = request.files[0];
-  let key = `${file.filename}.${file.originalname}`;
+  const file = request.files[0];
+  const key = `${file.filename}.${file.originalname}`;
+  console.log(file);
+  const tempFilePath = '../../temp/transform-temp.wav';
+  let bitCrushed = null;
+
+  return fsx.readFile(file) 
+    .then(data => {
+      const parsedFile = waveParser(data);
+      if(request.params.transform === 'bitcrusher') {
+        bitCrushed = bitCrusher(parsedFile);
+      }
+      return fsx.writeFile(tempFilePath, bitCrushed)
+        .then(() => {
+          return S3.upload(tempFilePath, key)
+            .then(url => {
+              return new Wave({
+                wavename: request.body.wavename,
+                user: request.user._id,
+                url,
+              }).save();
+            })
+            .then(wave => response.json(wave)) //TODO: can change this to download or other response method? download wave.url?
+            .catch(next);
+        });
+    });
+   
+
+  
 
   //check to see what the file binary stream is from multer and create a new variable to pass to wave parser
 
@@ -36,15 +64,6 @@ waveRouter.post('/waves', bearerAuth, upload.any(), (request, response, next) =>
 
   // we need to save the file here and get the new file path from where we are temporarily saving our written transformed file.
 
-  return S3.upload(file.path, key)
-    .then(url => {
-      return new Wave({
-        wavename: request.body.wavename,
-        user: request.user._id,
-        url,
-      }).save();
-    })
-    .then(wave => response.json(wave)) //TODO: can change this to download or other response method? download wave.url?
-    .catch(next);
+  
 });
 // });
