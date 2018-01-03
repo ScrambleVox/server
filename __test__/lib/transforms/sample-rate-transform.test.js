8103ba61afdc2fb54e5572da8677b3c14825bb5f
David Lindahl <davidalindahl@gmail.com>
2018-01-02 16:16:39 -0800

    more updates to the sample rate transform test

--- __test__/lib/transforms/sample-rate-transform.test.js
+++ __test__/lib/transforms/sample-rate-transform.test.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const waveParser = require('../../../lib/transforms/wave-parser');
+const waveWriter = require('../../../lib/transforms/wave-writer');
+const sampleRateTransform = require('../../../lib/transforms/sample-rate-transform');
+
+
+describe('sample-rate transform', () => {
+  test('the file should output with a sample rate that is half the input sample', done => {
+    const inputFilePath = `${__dirname}/../../assets/testclip.wav`;
+    
+    waveParser.getFile(inputFilePath, (error, data) => {
+      let constructedWaveFile = data;
+      sampleRateTransform(constructedWaveFile, (error, data) => {
+        expect(constructedWaveFile.sampleRate).toBe(4000);
+        waveWriter.writeFile('./temp/sample-rate-output.wav', constructedWaveFile, (error) => {
+          if(error) {
+            console.error(error);
+            return;
+          }
+        });
+      });
+    });
+
+    done();
+  });
+
+});
