79f66e21f735d632187e37846cc0febb5a75ac37
Andrew Bloom <blooma116@gmail.com>
2018-01-03 22:59:09 -0800

    adding code to enforce one file per user in db and tests for that code

--- __test__/wave-router.test.js
+++ __test__/wave-router.test.js
@@ -35,6 +35,24 @@ describe('Wave router', () => {
         });
     });
 
+    test('POST /waves/bitcrusher should only return the last posted file if user has existing file in database', () => {
+      let tempWaveMock = null;
+      return waveMock.create()
+        .then(mock => {
+          tempWaveMock = mock;
+          return superagent.post(`${__API_URL__}/waves/bitcrusher`)
+            .set('Authorization', `Bearer ${tempWaveMock.userMock.token}`)
+            .field('wavename', 'cornsilk')
+            .attach('wave', `${__dirname}/assets/testclip.wav`)
+            .then(response => {
+              expect(response.status).toEqual(200);
+              expect(response.body.wavename).toEqual('cornsilk');
+              expect(response.body._id).toBeTruthy();
+              expect(response.body.url).toBeTruthy();
+            });
+        });
+    });
+
     test('POST /waves/bitcrusher should return a 400 status if there is a bad request', () => {
       let tempUserMock = null;
       return userMock.create()
