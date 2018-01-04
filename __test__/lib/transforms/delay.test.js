67e25215b6515cba78d8a77ff4c0d1b33947d893
Andrew Bloom <blooma116@gmail.com>
2018-01-04 12:37:14 -0800

    changing bitcrusher to change to 3b rather than 2b, increasing delay feedback

--- __test__/lib/transforms/delay.test.js
+++ __test__/lib/transforms/delay.test.js
@@ -10,12 +10,28 @@ describe('testing delay transform module', () => {
     file.data = Buffer.from([255, 0, 127, 137]);
     delay(file);
     expect(file.data[0]).toEqual(255);
-    expect(file.data[1]).toEqual(63);
-    expect(file.data[2]).toEqual(111);
-    expect(file.data[3]).toEqual(130);
+    expect(file.data[1]).toEqual(127);
+    expect(file.data[2]).toEqual(127);
+    expect(file.data[3]).toEqual(134);
   });
 
   test('expect that delay changes values for 16 bit file', () => {
+    const bigFile = {};
+    bigFile.bitsPerSample = 16;
+    bigFile.sampleRate = 20;
+    bigFile.data = Buffer.from(new Uint16Array(8));
+    bigFile.data.writeUInt16LE(65535, 0);
+    bigFile.data.writeUInt16LE(0, 2);
+    bigFile.data.writeUInt16LE(32767, 4);
+    bigFile.data.writeUInt16LE(33767, 6);
+    delay(bigFile);
+    expect(bigFile.data.readUInt16LE(0)).toEqual(65535);
+    expect(bigFile.data.readUInt16LE(2)).toEqual(32767);
+    expect(bigFile.data.readUInt16LE(4)).toEqual(32767);
+    expect(bigFile.data.readUInt16LE(6)).toEqual(33517);
+  });
+  
+  test('expect that delay still works correctly for 16 bit file when odd sample amount given', () => {
     const bigFile = {};
     bigFile.bitsPerSample = 16;
     bigFile.sampleRate = 10;
@@ -26,8 +42,8 @@ describe('testing delay transform module', () => {
     bigFile.data.writeUInt16LE(33767, 6);
     delay(bigFile);
     expect(bigFile.data.readUInt16LE(0)).toEqual(65535);
-    expect(bigFile.data.readUInt16LE(2)).toEqual(16383);
-    expect(bigFile.data.readUInt16LE(4)).toEqual(28671);
-    expect(bigFile.data.readUInt16LE(6)).toEqual(32493);
+    expect(bigFile.data.readUInt16LE(2)).toEqual(32767);
+    expect(bigFile.data.readUInt16LE(4)).toEqual(32767);
+    expect(bigFile.data.readUInt16LE(6)).toEqual(33517);
   });
 });
