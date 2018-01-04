dc9ba7b79e1085c5013ec86bc41510fea6034385
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-03 19:27:51 -0800

    finish reverse transform

--- __test__/lib/transforms/reverse.test.js
+++ __test__/lib/transforms/reverse.test.js
@@ -14,18 +14,18 @@ describe(`reverse transform`, () => {
     expect(file.data[3]).toEqual(248);
   });
 
-  // test('expect that reverse transform changes values for 16 bit file', () => {
-  //   const bigFile = {};
-  //   bigFile.bitsPerSample = 16;
-  //   bigFile.data = Buffer.from(new Uint16Array(8));
-  //   bigFile.data.writeUInt16LE(50000, 0);
-  //   bigFile.data.writeUInt16LE(33000, 2);
-  //   bigFile.data.writeUInt16LE(20000, 4);
-  //   bigFile.data.writeUInt16LE(100, 6);
-  //   reverse(bigFile);
-  //   expect(bigFile.data.readUInt16LE(0)).toEqual(49152);
-  //   expect(bigFile.data.readUInt16LE(2)).toEqual(32768);
-  //   expect(bigFile.data.readUInt16LE(4)).toEqual(16384);
-  //   expect(bigFile.data.readUInt16LE(6)).toEqual(0);
-  // });
+  test('expect that reverse transform changes values for 16 bit file', () => {
+    const bigFile = {};
+    bigFile.bitsPerSample = 16;
+    bigFile.data = Buffer.from(new Uint16Array(8));
+    bigFile.data.writeUInt16LE(50000, 0);
+    bigFile.data.writeUInt16LE(33000, 2);
+    bigFile.data.writeUInt16LE(20000, 4);
+    bigFile.data.writeUInt16LE(100, 6);
+    reverse(bigFile);
+    expect(bigFile.data.readUInt16LE(0)).toEqual(100);
+    expect(bigFile.data.readUInt16LE(2)).toEqual(20000);
+    expect(bigFile.data.readUInt16LE(4)).toEqual(33000);
+    expect(bigFile.data.readUInt16LE(6)).toEqual(50000);
+  });
 });
